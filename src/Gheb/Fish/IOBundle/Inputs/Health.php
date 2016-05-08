937785c4a9459e6530615455fea54920cf53ffd5
Grégoire HébertÃ <gregoirehebert@gheb.fr>
2016-05-08 14:17:34 +0200

    feat (io) add health input

---
+++ src/Gheb/Fish/IOBundle/Inputs/Health.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Gheb\Fish\IOBundle\Inputs;
+
+use Gheb\Fish\FishBundle\Entity\Fish;
+use \DateTime;
+use Gheb\Fish\IOBundle\Inputs\AbstractInput;
+
+/**
+ * Class Health
+ * @author  Grégoire Hébert <gregoire@opo.fr>
+ * @package Gheb\Fish\IOBundle\Inputs
+ */
+class Health extends AbstractInput
+{
+    public function getName()
+    {
+        return 'health';
+    }
+
+    /**
+     * get the Health
+     */
+    public function getValue()
+    {
+        $this->getFish();
+        return $this->fish->getHealth();
+    }
+}
